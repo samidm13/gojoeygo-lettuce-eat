5fa67d8d837b2eba2d651f87a1c4e4d757fcf846
christymasterson <christymasterson@gmail.com>
2020-02-18 14:15:14 +0000

    Set up hello world in Gin

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", showIndexPage)
+	return router
+}
 
 func main() {
-  fmt.Printf("hello, world\n")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+	router := setupRouter()
+	router.Run(":" + port)
+}
+
+func showIndexPage(c *gin.Context) {
+	c.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{
+			"title": "Home Page",
+		},
+	)
 }
